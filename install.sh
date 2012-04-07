533e9b292a57a3a3cd371ac863f80cfa6a5a9a1d
Mauro Fruet <maurofruet@gmail.com>
2012-04-07 15:51:41 +0200

    Fixed removal of non-existing folder

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ program="dbsg"
 install -Dm755 $program /usr/bin/$program
 gzip -c $program.1 > $program.1.gz
 install -Dm644 $program.1.gz /usr/share/man/man1/$program.1.gz
+install -Dm777 /dev/null /var/log/$program.log
