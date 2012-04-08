905dcc9b760ea6ab27e03d410b473db82d7133f8
Mauro Fruet <maurofruet@gmail.com>
2012-04-08 10:18:29 +0200

    Fixed #2, added comments

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-program="dbsg"
+PROGRAM="dbsg"
 
-install -Dm755 $program /usr/bin/$program
-gzip -c $program.1 > $program.1.gz
-install -Dm644 $program.1.gz /usr/share/man/man1/$program.1.gz
-install -Dm777 /dev/null /var/log/$program.log
+install -Dm755 "$PROGRAM" "/usr/bin/$PROGRAM"
+gzip -c "$PROGRAM.1" > "$PROGRAM.1.gz"
+install -Dm644 "$PROGRAM.1.gz" "/usr/share/man/man1/$PROGRAM.1.gz"
